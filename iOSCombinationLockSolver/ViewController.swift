a1f9cbca13779c3e36102dc7039cd8ebde8adc8e
Pawel Bednarczyk <p.bednarczyk@icloud.com>
2016-02-08 23:35:32 +0100

    3 inputs label button and simple action

--- iOSCombinationLockSolver/ViewController.swift
+++ iOSCombinationLockSolver/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var XField: UITextField?
+    @IBOutlet weak var YField: UITextField?
+    @IBOutlet weak var ZField: UITextField?
+    
+    @IBOutlet weak var solveLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        XField?.becomeFirstResponder()
     }
 
+    @IBAction func updateSolveLabel() {
+        solveLabel?.text = "2 turns to the right, then to the 13, 1 turn left, to the 35, and then right again to the 18"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
