5b418e891dd5d2624e02370e097d4b2a428c97bc
Pawel Bednarczyk <p.bednarczyk@icloud.com>
2016-02-10 23:24:08 +0100

    Calculations

--- iOSCombinationLockSolver/ViewController.swift
+++ iOSCombinationLockSolver/ViewController.swift
@@ -18,13 +18,69 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        XField?.text = "88"
+        YField?.text = "54"
+        ZField?.text = "26"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     
         XField?.becomeFirstResponder()
     }
 
     @IBAction func updateSolveLabel() {
-        solveLabel?.text = "2 turns to the right, then to the 13, 1 turn left, to the 35, and then right again to the 18"
+        
+        let x = Int((XField?.text)!)!
+        let y = Int((YField?.text)!)!
+        let z = Int((ZField?.text)!)!
+        
+        let solve = solveThreeValues([x,y,z])
+        
+        solveLabel?.text = "\(solve))"
+        
+    }
+    
+    func getRotationWord (rotation: Int) -> String{
+        
+        switch(rotation) {
+        case 1:
+            return "obrót";
+        case 2,3,4:
+            return "obroty";
+        default:
+            return "obrotów";
+        }
+    }
+    
+    func getSolutionData(nNumber: Int, lastNumber: Int, body: Int, way: Bool) -> (Int, String, Bool, Int){
+  
+        let rotation = Int(floor(Double(nNumber) / Double(body)))
+        
+        var number = (way ? lastNumber - nNumber : (lastNumber + nNumber)) % body
+        
+        if number < 0 {
+            number += body
+        }
+        let rWord = getRotationWord(rotation)
+
+        return  (rotation: rotation, rWord: rWord, way: way, number: number)
+    
+    }
+    
+    func solveThreeValues(numbers: [Int]) -> [(rotation: Int, rWord: String, way: Bool, number: Int)]  {
+        
+        // another informations
+        let body = 40;
+        let way = true;
+        
+        // solution
+        var solution:[(rotation: Int, rWord: String, way: Bool, number: Int)] = []
+        
+        for var i = 0; i < numbers.count; i++ {
+            solution += [getSolutionData(numbers[i], lastNumber: ((i > 0) ? solution[i-1].number : 0), body: body, way: ((i > 0) ? !solution[i-1].way : way))]
+        }
+        return solution
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,6 +88,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
 
 }
 
